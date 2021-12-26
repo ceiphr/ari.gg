7166c808fc64ec36ff1a7b39f20593aa0bcb5731
Ari <ari@ceiphr.com>
2021-12-25 20:49:34 -0500

    Added typekit support.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,19 @@
+require("dotenv").config();
+
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
     title: "ari.gg",
   },
   plugins: [
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        typekit: {
+          id: process.env.TYPEKIT_ID,
+        },
+      },
+    },
     "gatsby-plugin-netlify-cms",
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
