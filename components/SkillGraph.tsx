1f923dd0fda4843b541a7b8bfca9d34779e209bf
Ari <ari@ceiphr.com>
2021-12-31 16:08:09 -0500

    Trying to fix drag on mobile issue.

--- components/SkillGraph.tsx
+++ components/SkillGraph.tsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useRef } from "react";
 // @ts-ignore
-import { ForceGraph3D, GraphNode, GraphData, ForceGraphInstance } from "react-force-graph";
+import {
+  ForceGraph3D,
+  GraphNode,
+  GraphData,
+  ForceGraphInstance,
+} from "react-force-graph";
 import * as THREE from "three";
 
 const SkillGraph = ({ data }: GraphData) => {
@@ -27,25 +32,27 @@ const SkillGraph = ({ data }: GraphData) => {
   }, []);
 
   return (
-    <ForceGraph3D
-      ref={fgRef}
-      graphData={data}
-      backgroundColor={"#fff"}
-      linkColor={() => "rgba(0,0,0,0.6)"}
-      linkWidth={0.5}
-      d3AlphaDecay={0.06}
-      enableNodeDrag={false}
-      enableNavigationControls={false}
-      showNavInfo={false}
-      nodeThreeObject={({ img }: GraphNode) => {
-        const imgTexture = new THREE.TextureLoader().load(`${img}`);
-        const material = new THREE.SpriteMaterial({ map: imgTexture });
-        const sprite = new THREE.Sprite(material);
+    <div className="pointer-events-none">
+      <ForceGraph3D
+        ref={fgRef}
+        graphData={data}
+        backgroundColor={"#fff"}
+        linkColor={() => "rgba(0,0,0,0.6)"}
+        linkWidth={0.5}
+        d3AlphaDecay={0.06}
+        enableNodeDrag={false}
+        enableNavigationControls={false}
+        showNavInfo={false}
+        nodeThreeObject={({ img }: GraphNode) => {
+          const imgTexture = new THREE.TextureLoader().load(`${img}`);
+          const material = new THREE.SpriteMaterial({ map: imgTexture });
+          const sprite = new THREE.Sprite(material);
 
-        sprite.scale.set(12, 12, 1);
-        return sprite;
-      }}
-    />
+          sprite.scale.set(12, 12, 1);
+          return sprite;
+        }}
+      />
+    </div>
   );
 };
 
