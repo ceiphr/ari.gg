91be2c7d3ea0d0e468390b6131df3b763c26eb0b
Ari <ari@ceiphr.com>
2022-01-05 20:03:51 -0500

    Polishing margins for mobile.

--- components/projects/ProjectList.tsx
+++ components/projects/ProjectList.tsx
@@ -9,7 +9,7 @@ const ProjectList = () => {
       <div className="md:col-start-2 md:col-span-2 p-4 md:p-0 mb-12 z-30">
         <div className="backdrop--contrast mb-8">
           <Fade bottom>
-            <h1 className="tk-neue-haas-grotesk-display text-clamp-5xl uppercase md:-ml-px mb-2">
+            <h1 className="tk-neue-haas-grotesk-display text-clamp-5xl uppercase -ml-3px md:-ml-px mb-2">
               Projects
             </h1>
             <p className="hidden md:inline md:-ml-px text-stone-600 dark:text-stone-400 text-xl">
